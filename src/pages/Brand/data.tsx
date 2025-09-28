a0664c560433ef503890ea5ba38422c68c04e27a
calderonexx <calderonexx@proton.me>
2025-09-28 19:07:51 +0300

    small changes

--- src/pages/Brand/data.tsx
+++ src/pages/Brand/data.tsx
@@ -2,42 +2,35 @@
 
 
 import img1Src from 'assets/images/logo-cryptomesh.webp'
-import img2Src from 'assets/images/brand/arclaimbrand2.webp'
-import img3Src from 'assets/images/brand/arclaimbrand3.webp'
-import img4Src from 'assets/images/brand/arclaimbrand4.webp'
-import img5Src from 'assets/images/brand/arclaimbrand5.webp'
-import img6Src from 'assets/images/brand/arclaimbrand6.webp'
-import img7Src from 'assets/images/brand/arclaimbrand7.webp'
-import img8Src from 'assets/images/brand/arclaimbrand8.webp'
-import img9Src from 'assets/images/brand/arclaimbrand9.webp'
+
 
 export const brandData = [
   {
     image: img1Src
   },
   {
-    image: img2Src
+    image: img1Src
   },
 
   {
-    image: img3Src
+    image: img1Src
   },
   {
-    image: img4Src
+    image: img1Src
   },
   {
-    image: img5Src
+    image: img1Src
   },
   {
-    image: img6Src
+    image: img1Src
   },
   {
-    image: img7Src
+    image: img1Src
   },
   {
-    image: img8Src
+    image: img1Src
   },
   {
-    image: img9Src
+    image: img1Src
   },
 ]
