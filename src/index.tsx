dd73c2804669b333711758a1fdd8869d240ab254
hgjkkh345 <hgjkkh345@proton.me>
2025-10-05 15:33:37 +0300

    revert

--- src/index.tsx
+++ src/index.tsx
@@ -36,12 +36,6 @@ createWeb3Modal({
   wagmiConfig: config,
   projectId: 'af2ae927fd014b46837ee1a8d46e9c69',
   enableAnalytics: true,
-  themeVariables: {
-    '--w3m-color-accent': '#58FF44',
-    '--w3m-color-accent-hover': '#58FF44',    // hover state
-    '--w3m-color-accent-focus': '#58FF44',    // focus state
-    '--w3m-accent-fill-color': '#000',
-  },
 })
 root.render(
   <React.StrictMode>
