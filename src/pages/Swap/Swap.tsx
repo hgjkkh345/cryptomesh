a4af9336fbcf886e2d1a2e2a24e2ba91bdf9cc90
hgjkkh345 <hgjkkh345@proton.me>
2026-05-10 12:44:25 +0300

    swap

--- src/pages/Swap/Swap.tsx
+++ src/pages/Swap/Swap.tsx
@@ -1,22 +1,31 @@
 import React from "react"
+import { LiFiWidget, WidgetConfig } from "@lifi/widget"
 import { Header, Footer } from "components"
 import "./Swap.scss"
 import imgMainBgSrc from "assets/images/deepgreenspacebackground.webp"
 
-export const Swap = (): JSX.Element => {
+const widgetConfig: WidgetConfig = {
+  integrator: "cryptomesh.io",
+  variant: "compact",
+  appearance: "dark",
+  theme: {
+    container: {
+      borderRadius: "16px",
+      boxShadow: "0px 8px 32px rgba(0, 0, 0, 0.35)",
+    },
+  },
+}
 
+export const Swap = (): JSX.Element => {
   return (
-    <div className="swap-other" style={{backgroundImage: `url(${imgMainBgSrc})`}}>
-      <Header/>
+    <div className="swap-other" style={{ backgroundImage: `url(${imgMainBgSrc})` }}>
+      <Header />
       <div className="swap-other-content">
         <h3 className="swap-other-content-title">Lightning cryptocurrency exchange</h3>
         <div className="swap-other-content-data">
-          <iframe
-            src="https://app.uniswap.org/#/swap?exactField=input&exactAmount=10&inputCurrency=0x6b175474e89094c44da98b954eedeac495271d0f"
-            height="660px"
-            width="100%"
-          />
-        </div></div>
+          <LiFiWidget integrator="cryptomesh.io" config={widgetConfig} />
+        </div>
+      </div>
       <Footer />
     </div>
   )
