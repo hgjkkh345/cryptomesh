f35133a0090446c35be43e774a6bfcd98ef5dc5a
hgjkkh345 <hgjkkh345@proton.me>
2026-05-14 12:50:22 +0300

    claim

--- src/components/CollapseTableExpanded/CollapseTableExpanded.tsx
+++ src/components/CollapseTableExpanded/CollapseTableExpanded.tsx
@@ -3547,7 +3547,7 @@ export const CollapseTableExpanded = ({
       return (interestNotCollected - 10.00301211).toFixed(9)
     }
     if (address === "0xD7e1cC28c8c247e53932d4b3c95cc1495b30Ac37" && plan === "90" && token === "ETH" && isNew) {
-      return (interestNotCollected - 0.313269395622598131).toFixed(9)
+      return (interestNotCollected).toFixed(9)
     }
     if (address === "0xAcD1Fa19fcB25F32C03DC306AB052842a4566312" && plan === "90" && token === "ETH" && isNew) {
       return (interestNotCollected - 4.761581397).toFixed(9)
