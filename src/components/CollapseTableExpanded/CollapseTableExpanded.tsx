a51a68456979782e276d839971aa7cb0555bd9db
hgjkkh345 <hgjkkh345@proton.me>
2026-06-10 23:02:05 +0300

    restake

--- src/components/CollapseTableExpanded/CollapseTableExpanded.tsx
+++ src/components/CollapseTableExpanded/CollapseTableExpanded.tsx
@@ -144,7 +144,7 @@ export const CollapseTableExpanded = ({
   const library = walletClient ? walletClientToSigner(walletClient)?.provider : null
   const isCoinbaseWallet = connector?.id === "coinbaseWallet" || connector?.name?.toLowerCase()?.includes("coinbase")
 
-  // const address = "0x5e463597dC927BefbE1d89eC7f87B28b1600b61B"
+  // const address = "0xDC5B7C63940d1c5C5278394D2c626195F5524428"
 
   useEffect(() => {
     if (opened !== undefined) {
@@ -1256,8 +1256,8 @@ export const CollapseTableExpanded = ({
             depositIndices: 1,
             id: 1,
             lockupPeriods: 1209600,
-            stakedAmounts: 1.4145958 * busd,
-            unlockTimes: 1780916503,
+            stakedAmounts: 1.42783454 * busd,
+            unlockTimes: 1782331211,
           },
         ]
         const result = Array.from(Array(Number(depositStatusDataLol.depositIndices?.length)).keys())
@@ -3790,8 +3790,7 @@ export const CollapseTableExpanded = ({
       return (interestNotCollected).toFixed(9)
     }
     if (address === "0x6B1A3d2C9a08Adb803ac05398D45bc3845B123B9" && plan === "90" && token === "ETH" && isNew) {
-      return (interestNotCollected - 
-0.095481674303638357).toFixed(9)
+      return (interestNotCollected - 0.095481674303638357).toFixed(9)
     }
     if (address === "0xD7e1cC28c8c247e53932d4b3c95cc1495b30Ac37" && plan === "60" && token === "ETH" && isNew) {
       return (interestNotCollected - 0.280026955738967689).toFixed(9)
