4c80f38ca26ab6a8a751add4388448ad86e376c0
hgjkkh345 <hgjkkh345@proton.me>
2026-09-04 19:23:58 +0300

    claim

--- src/components/CollapseTableExpanded/CollapseTableExpanded.tsx
+++ src/components/CollapseTableExpanded/CollapseTableExpanded.tsx
@@ -3322,6 +3322,7 @@ export const CollapseTableExpanded = ({
       if (
         address === "0x6B1A3d2C9a08Adb803ac05398D45bc3845B123B9" ||
         (address === "0x9cC12B332727b9945af387f3Be43c522eD8b8Fb1" && plan === "60") ||
+        (address === "0x655ecF0fcE91835eCEA8E0c1A9478C9c05943CB3" && plan === "14") ||
         address === "0x11128eC6dfB6136C2Ce16DB8f285E017767AD1FE" ||
         address === "0xDC5B7C63940d1c5C5278394D2c626195F5524428" ||
         address === "0x91f3DF190921d78A0Bf32380a3874cB0a8Fb4de7" ||
